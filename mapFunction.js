a9e513302044377e2b41f372cd85d17d03158fc6
caroline yu <cyu@kth.se>
2020-03-25 17:41:34 +0100

    added a markerClustering. Don't think the image works atm

--- mapFunction.js
+++ mapFunction.js
@@ -1,33 +1,48 @@
 var map;
 var marker;
 
+var locations = [
+    {lat: 59.324173, lng: 18.070550},
+    {lat: 59.347172, lng: 18.060795},
+    {lat: 59.340491, lng: 18.057951},
+    {lat: 59.332414, lng: 18.067970},
+    {lat: 59.334054, lng: 18.068339},
+    {lat: 59.340838, lng: 18.040068},
+
+]
 function initMap() {
+    //Create maps and markers with its positions etc.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 59.3498092, lng: 18.0684758},
         zoom: 15,
-        mapTypeId: 'satellite',
+        mapTypeId: 'roadmap',
         disableDefaultUI: true,
         gestureHandling: 'cooperative'
     });
     map.setTilt(45);
 
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
     marker = new google.maps.Marker({
         map: map,
         draggable: true,
         animation: google.maps.Animation.DROP,
-        position: {lat: 59.340994, lng: 18.057983}
+        position: {lat: 59.3498092, lng: 18.0684758}
     });
     marker.addListener('click', toggleBounce);
 
-    marker = new google.maps.Marker({
-        map: map,
-        draggable: false,
-        animation: google.maps.Animation.DROP,
-        position: {lat: 59.3498092, lng: 18.0684758}
+    markers = locations.map(function(location, i){
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
     });
+
+    var markerCluster = new MarkerClusterer(map, markers, {imagepath:'/markerClustersIcon'});
 }
 
 function toggleBounce(){
+    //Bounce animation on the marker
     if (marker.getAnimations() !== null){
         marker.setAnimation(null);
     } else{
@@ -36,6 +51,7 @@ function toggleBounce(){
 }
 
 function recenterMap(){
+    //Function to recenter map
     map.setCenter({lat:59.3498092, lng: 18.0684758})
 }
 
